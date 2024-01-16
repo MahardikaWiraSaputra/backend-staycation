7d3e5352444d909ececa09d24fef57f0a0a1a89f
mahardikaws <mahardikawsp@gmail.com>
2024-01-16 10:12:36 +0700

    api finish

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,9 @@
 const router = require('express').Router(); // menggunakan router dari express
 const apiController = require('../controllers/apiController');
+const { upload} = require('../middlewares/mutler')
 
 router.get('/landing-page', apiController.landingPage);
+router.get('/detail-page/:id', apiController.detailPage);
+router.post('/booking-page', upload, apiController.bookingPage);
 
 module.exports = router;
